6df7021bc18601bdd62b199bc3a3e4c7eaef4cc1
Tygo <tygo.geervliet@hva.nl>
2022-11-30 13:37:12 +0100

    connection db profile page. POST AND GET REQUESTS

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -3,57 +3,75 @@ Tygo
 Javascript script for POST reqeust from profile page to edit profile page
 code could be made cleaner ,but it works ;)
 */
+var query = "SELECT user.userID, user.firstName, user.lastName, user.email, userinfo.nationality  FROM user, userinfo WHERE user.userID = userinfo.userID;";
+var userID = 1;
 
 let form = document.getElementById('form1');
 form?.addEventListener("submit", function (e) {
     e.preventDefault();
-    CreateUrl(1, "lala", "land", "gender", "leeftijdcategorie", "beschrijving");
+    var submittedValues = {};
+        UpdateDB(1,submittedValues);
 })
-
 function updateProfile(arr, formtype) {
 
-    var test = FYSCloud.API.queryDatabase("SELECT * FROM user WHERE userID = 1;").value;
+     FYSCloud.API.queryDatabase
+        (query).then(function(data) {
+         let counter = 1;
+         for (const [key, value] of Object.entries(data[userID -1])) {
 
-    console.log(test);
-
-
-
+             if(counter != 0) {
+             if(counter <= arr.length) {
+                     if (formtype == 1 && !key.includes('isAdmin')) {
+                         document.getElementById(arr[counter-1]).innerHTML = value;
+                         console.log(arr[counter-1] + value)
+                     }
+                      if (formtype == 2 && !key.includes('isAdmin')) {
+                         document.getElementById(arr[counter-1]).value = value;
+                      }
+             }
+             }
+             counter++;
+         }
+        }
+    );
 }
-
 //EventListener submit
 form = document.getElementById('form2');
 form?.addEventListener("submit", function (e) {
     e.preventDefault();
     active = 2;
-    CreateUrl(2, "nameInput", "land", "gender", "leeftijdcategorie", "beschrijving");
+    var firstname = document.querySelector("#name").value;
+    var submittedValues = {
+        firstName: firstname
+    };
+    UpdateDB(2,submittedValues);
 })
 
-function CreateUrl(formNum, namePar, landPar, genderPar, leeftijdcategoriePar, beschrijvingPar) {
+function UpdateDB(formNum, ObjDataCurrentUser) {
 
-    const name = document.getElementById(namePar).value;
-    const land = document.getElementById(landPar).value;
-    const gender = document.getElementById(genderPar).value;
-    const ageCatagory = document.getElementById(leeftijdcategoriePar).value;
-    const beschrijving = document.getElementById(beschrijvingPar).value;
+    var firstName = ObjDataCurrentUser.firstName
+    console.log(firstName)
 
-    //redirect to pages
     if (formNum === 1) {
-        window.location.href = "EditProfile.html" ;
-    } else {
+           window.location.href = "EditProfile.html" ;
+         } else {
 
-        // query om message in database te zetten
         FYSCloud.API.queryDatabase(
-            "UPDATE user SET firstName = (?) WHERE userID = 1;", [name]
+            "UPDATE user SET firstName = (?) WHERE userID = 1;", [firstName]
         )
-
         window.location.href = "ProfilePage.html";
     }
 }
-
 //form type 1 = text elements
 //form type 2 = input fields
-const TextID = ["TitleName", "landText", "genderText", "leeftijdcategorieText", "BeschrijvingText"];
-const TextID2 = ["nameInput", "land", "gender", "leeftijdcategorie", "beschrijving"];
+
+//profilepage ID names //form type 1
+const TextID = ["userID", "nameText", "lastNameText", "emailText", "land"];
+
+//edit profile page ID names //form type 2
+const TextID2 = ["userIDInput", "name"];
+
+
 if (location.href.includes("EditProfile")) {
     updateProfile(TextID2, 2);
 } else {
