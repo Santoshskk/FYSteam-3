31c0655fa60b805e5cd25440c13c3c3b0a1de3ea
Tygo <tygo.geervliet@hva.nl>
2022-12-05 11:47:02 +0100

    profile page user profile update works update profile img. cant update to db

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,17 +1,18 @@
 /*
 Tygo
-Javascript script for POST reqeust from profile page to edit profile page
+Javascript script for POST request from profile page to edit profile page
 code could be made cleaner ,but it works ;)
 */
-var query = "SELECT user.userID, user.firstName, user.lastName, user.email, userinfo.nationality  FROM user, userinfo WHERE user.userID = userinfo.userID;";
-var userID = 1;
+let query = "SELECT user.userID, user.firstName, user.lastName, user.email, userinfo.nationality  FROM user, userinfo WHERE user.userID = userinfo.userID;";
+const userID = FYSCloud.Session.get("userId");
 
 let form = document.getElementById('form1');
 form?.addEventListener("submit", function (e) {
     e.preventDefault();
-    var submittedValues = {};
-        UpdateDB(1,submittedValues);
+    let submittedValues = {};
+    UpdateDB(1,submittedValues);
 })
+
 function updateProfile(arr, formtype) {
 
      FYSCloud.API.queryDatabase
@@ -19,16 +20,14 @@ function updateProfile(arr, formtype) {
          let counter = 1;
          for (const [key, value] of Object.entries(data[userID -1])) {
 
-             if(counter != 0) {
              if(counter <= arr.length) {
-                     if (formtype == 1 && !key.includes('isAdmin')) {
+                     if (formtype == 1) {
                          document.getElementById(arr[counter-1]).innerHTML = value;
                          console.log(arr[counter-1] + value)
                      }
-                      if (formtype == 2 && !key.includes('isAdmin')) {
+                     else {
                          document.getElementById(arr[counter-1]).value = value;
                       }
-             }
              }
              counter++;
          }
@@ -38,42 +37,63 @@ function updateProfile(arr, formtype) {
 //EventListener submit
 form = document.getElementById('form2');
 form?.addEventListener("submit", function (e) {
+
     e.preventDefault();
-    active = 2;
-    var firstname = document.querySelector("#name").value;
-    var submittedValues = {
-        firstName: firstname
-    };
-    UpdateDB(2,submittedValues);
+    const firstname = document.querySelector("#name").value;
+    const lastname = document.querySelector("#lastName").value;
+    const email = document.querySelector("#email").value;
+    const nationality = document.querySelector("#nationality").value;
+    let newProfileImage;
+
+    FYSCloud.Utils
+        .getDataUrl("#fileUpload")
+        .then(function(data) {
+
+            newProfileImage = data.url;
+            console.log(data)
+            const submittedValues = {
+                firstName: firstname,
+                lastName: lastname,
+                email: email,
+                nationality: nationality,
+                profileImage: newProfileImage
+            };
+
+            UpdateDB(2,submittedValues);
+
+        })
+        .catch(function(reason) {
+            console.log(reason)
+        });
 })
 
 function UpdateDB(formNum, ObjDataCurrentUser) {
 
-    var firstName = ObjDataCurrentUser.firstName
-    console.log(firstName)
+    const firstName = ObjDataCurrentUser.firstName;
+    const lastName = ObjDataCurrentUser.lastName
+    const email = ObjDataCurrentUser.email;
+    const nationality = ObjDataCurrentUser.nationality;
+    const profileImage = ObjDataCurrentUser.profileImage;
 
     if (formNum === 1) {
            window.location.href = "EditProfile.html" ;
          } else {
-
         FYSCloud.API.queryDatabase(
-            "UPDATE user SET firstName = (?) WHERE userID = 1;", [firstName]
+            "UPDATE user, userinfo SET user.firstName = (?), user.lastName = (?), user.email = (?), userinfo.nationality = (?), user.profileImage = (?) WHERE user.userID = 1;", [firstName,lastName,email,nationality,profileImage]
         )
-        window.location.href = "ProfilePage.html";
+       // window.location.href = "ProfilePage.html";
     }
 }
-//form type 1 = text elements
-//form type 2 = input fields
 
 //profilepage ID names //form type 1
 const TextID = ["userID", "nameText", "lastNameText", "emailText", "land"];
 
 //edit profile page ID names //form type 2
-const TextID2 = ["userIDInput", "name"];
-
+const TextID2 = ["userIDInput", "name", "lastName", "email", "nationality"];
 
 if (location.href.includes("EditProfile")) {
     updateProfile(TextID2, 2);
 } else {
     updateProfile(TextID, 1);
 }
+
