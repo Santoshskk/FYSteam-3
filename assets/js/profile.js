c1e137086d4d361d1508e8732adb1664fe653b7e
Tygo <tygo.geervliet@hva.nl>
2022-12-05 14:56:36 +0100

    FIXED PROFILE IMAGE

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -3,8 +3,8 @@ Tygo
 Javascript script for POST request from profile page to edit profile page
 code could be made cleaner ,but it works ;)
 */
-let query = "SELECT user.userID, user.firstName, user.lastName, user.email, userinfo.nationality  FROM user, userinfo WHERE user.userID = userinfo.userID;";
-const userID = FYSCloud.Session.get("userId");
+let query = "SELECT user.userID, user.firstName, user.lastName, user.email, userinfo.nationality, user.profileImage  FROM user, userinfo WHERE user.userID = userinfo.userID;";
+const userID = FYSCloud.Session.get("userID");
 
 let form = document.getElementById('form1');
 form?.addEventListener("submit", function (e) {
@@ -19,16 +19,21 @@ function updateProfile(arr, formtype) {
         (query).then(function(data) {
          let counter = 1;
          for (const [key, value] of Object.entries(data[userID -1])) {
+             console.log(value)
 
-             if(counter <= arr.length) {
                      if (formtype == 1) {
-                         document.getElementById(arr[counter-1]).innerHTML = value;
-                         console.log(arr[counter-1] + value)
-                     }
+                         if(key === "profileImage") {
+                             console.log(value)
+                             document.querySelector(".profileImg").src = value;
+                         }
+                         else {
+                             document.getElementById(arr[counter - 1]).innerHTML = value;
+                         }
+                         }
                      else {
                          document.getElementById(arr[counter-1]).value = value;
                       }
-             }
+
              counter++;
          }
         }
@@ -46,25 +51,47 @@ form?.addEventListener("submit", function (e) {
     let newProfileImage;
 
     FYSCloud.Utils
-        .getDataUrl("#fileUpload")
+        .getDataUrl(document.querySelector("#fileUpload"))
         .then(function(data) {
 
-            newProfileImage = data.url;
-            console.log(data)
-            const submittedValues = {
-                firstName: firstname,
-                lastName: lastname,
-                email: email,
-                nationality: nationality,
-                profileImage: newProfileImage
-            };
+            let name;
+            FYSCloud.API.listDirectory().then(function (list) {
+               name = "ImgNumber" + (list.length +1) + ".png";
 
-            UpdateDB(2,submittedValues);
+            FYSCloud.API.uploadFile(
+                name + ".png",
+                data.url
+            ).then(function(data) {
 
-        })
-        .catch(function(reason) {
-            console.log(reason)
-        });
+                console.log(data)
+                newProfileImage = data;
+                const submittedValues = {
+                    firstName: firstname,
+                    lastName: lastname,
+                    email: email,
+                    nationality: nationality,
+                    profileImage: newProfileImage
+                };
+                    UpdateDB(2, submittedValues);
+            }).catch(function(reason) {
+            });
+            })
+
+        }).catch(function(reason) {
+        const submittedValues = {
+            firstName: firstname,
+            lastName: lastname,
+            email: email,
+            nationality: nationality,
+            profileImage: null
+        };
+        UpdateDB(2, submittedValues);
+    });
+
+
+    //stop
+
+
 })
 
 function UpdateDB(formNum, ObjDataCurrentUser) {
@@ -81,7 +108,7 @@ function UpdateDB(formNum, ObjDataCurrentUser) {
         FYSCloud.API.queryDatabase(
             "UPDATE user, userinfo SET user.firstName = (?), user.lastName = (?), user.email = (?), userinfo.nationality = (?), user.profileImage = (?) WHERE user.userID = 1;", [firstName,lastName,email,nationality,profileImage]
         )
-       // window.location.href = "ProfilePage.html";
+        window.location.href = "ProfilePage.html";
     }
 }
 
