a008b8ac5e09f0c1b29b246ed1168ddce57ba8aa
Tygo <tygo.geervliet@hva.nl>
2022-12-05 15:00:44 +0100

    merge issue fix

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -3,7 +3,7 @@ Tygo
 Javascript script for POST request from profile page to edit profile page
 code could be made cleaner ,but it works ;)
 */
-let query = "SELECT user.userID, user.firstName, user.lastName, user.email, userinfo.nationality  FROM user, userinfo WHERE user.userID = userinfo.userID;";
+let query = "SELECT user.userID, user.firstName, user.lastName, user.email, userinfo.nationality, user.profileImage  FROM user, userinfo WHERE user.userID = userinfo.userID;";
 const userID = FYSCloud.Session.get("userID");
 
 let form = document.getElementById('form1');
@@ -15,22 +15,27 @@ form?.addEventListener("submit", function (e) {
 
 function updateProfile(arr, formtype) {
 
-     FYSCloud.API.queryDatabase
-        (query).then(function(data) {
-         let counter = 1;
-         for (const [key, value] of Object.entries(data[userID -1])) {
+    FYSCloud.API.queryDatabase
+    (query).then(function(data) {
+            let counter = 1;
+            for (const [key, value] of Object.entries(data[userID -1])) {
+                console.log(value)
 
-             if(counter <= arr.length) {
-                     if (formtype == 1) {
-                         document.getElementById(arr[counter-1]).innerHTML = value;
-                         console.log(arr[counter-1] + value)
-                     }
-                     else {
-                         document.getElementById(arr[counter-1]).value = value;
-                      }
-             }
-             counter++;
-         }
+                if (formtype == 1) {
+                    if(key === "profileImage") {
+                        console.log(value)
+                        document.querySelector(".profileImg").src = value;
+                    }
+                    else {
+                        document.getElementById(arr[counter - 1]).innerHTML = value;
+                    }
+                }
+                else {
+                    document.getElementById(arr[counter-1]).value = value;
+                }
+
+                counter++;
+            }
         }
     );
 }
@@ -45,29 +50,48 @@ form?.addEventListener("submit", function (e) {
     const nationality = document.querySelector("#nationality").value;
     let newProfileImage;
 
-
-    //upload image
-
     FYSCloud.Utils
-        .getDataUrl("#fileUpload")
+        .getDataUrl(document.querySelector("#fileUpload"))
         .then(function(data) {
 
-            newProfileImage = data.url;
-            console.log(data.url)
-            const submittedValues = {
-                firstName: firstname,
-                lastName: lastname,
-                email: email,
-                nationality: nationality,
-                profileImage: newProfileImage
-            };
+            let name;
+            FYSCloud.API.listDirectory().then(function (list) {
+                name = "ImgNumber" + (list.length +1) + ".png";
 
-            UpdateDB(2,submittedValues);
+                FYSCloud.API.uploadFile(
+                    name + ".png",
+                    data.url
+                ).then(function(data) {
 
-        })
-        .catch(function(reason) {
-            console.log(reason)
-        });
+                    console.log(data)
+                    newProfileImage = data;
+                    const submittedValues = {
+                        firstName: firstname,
+                        lastName: lastname,
+                        email: email,
+                        nationality: nationality,
+                        profileImage: newProfileImage
+                    };
+                    UpdateDB(2, submittedValues);
+                }).catch(function(reason) {
+                });
+            })
+
+        }).catch(function(reason) {
+        const submittedValues = {
+            firstName: firstname,
+            lastName: lastname,
+            email: email,
+            nationality: nationality,
+            profileImage: null
+        };
+        UpdateDB(2, submittedValues);
+    });
+
+
+    //stop
+
+
 })
 
 function UpdateDB(formNum, ObjDataCurrentUser) {
@@ -79,12 +103,12 @@ function UpdateDB(formNum, ObjDataCurrentUser) {
     const profileImage = ObjDataCurrentUser.profileImage;
 
     if (formNum === 1) {
-           window.location.href = "EditProfile.html" ;
-         } else {
+        window.location.href = "EditProfile.html" ;
+    } else {
         FYSCloud.API.queryDatabase(
             "UPDATE user, userinfo SET user.firstName = (?), user.lastName = (?), user.email = (?), userinfo.nationality = (?), user.profileImage = (?) WHERE user.userID = 1;", [firstName,lastName,email,nationality,profileImage]
         )
-       // window.location.href = "ProfilePage.html";
+        window.location.href = "ProfilePage.html";
     }
 }
 
