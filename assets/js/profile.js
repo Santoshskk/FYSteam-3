ab6159480e50dddc05129a8a3461fd852373efb9
Tygo <tygo.geervliet@hva.nl>
2022-12-06 14:29:06 +0100

    fixed profile page image and updating page

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -3,8 +3,9 @@ Tygo
 Javascript script for POST request from profile page to edit profile page
 code could be made cleaner ,but it works ;)
 */
-let query = "SELECT user.userID, user.firstName, user.lastName, user.email, userinfo.nationality, user.profileImage  FROM user, userinfo WHERE user.userID = userinfo.userID;";
 const userID = FYSCloud.Session.get("userID");
+let query = "SELECT user.userID, user.firstName, user.lastName, user.email, userinfo.nationality, user.profileImage  FROM user, userinfo WHERE user.userID = userinfo.userID;";
+
 
 let form = document.getElementById('form1');
 form?.addEventListener("submit", function (e) {
@@ -24,7 +25,12 @@ function updateProfile(arr, formtype) {
                 if (formtype == 1) {
                     if(key === "profileImage") {
                         console.log(value)
-                        document.querySelector(".profileImg").src = value;
+                        const defaultPic = "https://www.showflipper.com/blog/images/default.jpg";
+                        if(value != null) {
+                            document.querySelector(".profileImg").src = value;
+                        } else {
+                            document.querySelector(".profileImg").src = defaultPic;
+                        }
                     }
                     else {
                         document.getElementById(arr[counter - 1]).innerHTML = value;
@@ -103,12 +109,25 @@ function UpdateDB(formNum, ObjDataCurrentUser) {
     const profileImage = ObjDataCurrentUser.profileImage;
 
     if (formNum === 1) {
-        window.location.href = "EditProfile.html" ;
+        window.location.href = "EditProfile.html";
     } else {
-        FYSCloud.API.queryDatabase(
-            "UPDATE user, userinfo SET user.firstName = (?), user.lastName = (?), user.email = (?), userinfo.nationality = (?), user.profileImage = (?) WHERE user.userID = 1;", [firstName,lastName,email,nationality,profileImage]
-        )
-        window.location.href = "ProfilePage.html";
+
+        if (profileImage == null) {
+            FYSCloud.API.queryDatabase(
+                "UPDATE user, userinfo SET user.firstName = (?), user.lastName = (?), user.email = (?), userinfo.nationality = (?) WHERE user.userID = (?);", [firstName, lastName, email, nationality, userID]
+            ).then(function () {
+                window.location.href = "ProfilePage.html";
+            })
+        }
+
+        else {
+            FYSCloud.API.queryDatabase(
+                "UPDATE user, userinfo SET user.firstName = (?), user.lastName = (?), user.email = (?), userinfo.nationality = (?), user.profileImage = (?) WHERE user.userID = (?);", [firstName, lastName, email, nationality, profileImage, userID]
+            ).then(function () {
+                window.location.href = "ProfilePage.html";
+            })
+        }
+
     }
 }
 
