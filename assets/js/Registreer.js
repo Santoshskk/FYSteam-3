43446c40b7e2f03af26a3ced6f69b82d33e75eb1
othaimIbo <iboothaim@gmail.com>
2022-12-14 16:35:38 +0100

    Brought slight changes to the login & registration files

--- assets/js/Registreer.js
+++ assets/js/Registreer.js
@@ -20,16 +20,22 @@ document.addEventListener('DOMContentLoaded', function (e) {
 
         // Check if email already exists
         function EmailCheck() {
-            FYSCloud.API.queryDatabase(
+            const emailExists = FYSCloud.API.queryDatabase(
                 "SELECT user.email FROM user WHERE email = (?)", [email.value],
-            ).then(function (data) {
-                console.log(data)
-                if (data.length > 0) {
-                    return false;
-                } else {
-                    return true;
-                }
-            })
+            ).then(response => {
+                console.log("res: " + response);
+                response.json();
+            });
+
+
+            const printEmail = async () => {
+                const a = await emailExists;
+                console.log(a);
+            };
+
+            printEmail();
+
+
         }
 
         function SignUp() {
@@ -63,30 +69,62 @@ document.addEventListener('DOMContentLoaded', function (e) {
             const passwordValue = password.value.trim();
             const password2Value = password2.value.trim();
 
+            let countError = 0;
+
             if (firstnameValue === '') {
                 setErrorFor(firstname, 'Voornaam kan niet leeg zijn');
-            } else if (lastnameValue === '') {
-                setErrorFor(lastname, 'Achternaam kan niet leeg zijn')
-            } else if (emailValue === '') {
-                setErrorFor(email, 'Email kan niet leeg zijn');
-            } else if (!isEmail(emailValue)) {
-                setErrorFor(email, 'Ongeldige email adres');
-            }else if (!EmailCheck(emailValue)){
-                setErrorFor(email, 'Deze email is al geregistreerd, probeer in te loggen');
-            } else if (passwordValue === '') {
-                setErrorFor(password, 'Wachtwoord kan niet leeg zijn');
-            } else if (password2Value === '') {
-                setErrorFor(password2, 'Dit veld mag niet leeg zijn');
-            } else if (passwordValue !== password2Value) {
-                setErrorFor(password2, 'Wachtwoorden komen niet overeen');
+                countError++;
             } else {
                 setSuccessFor(firstname);
-                setSuccessFor(lastname)
+            }
+            if (lastnameValue === '') {
+                setErrorFor(lastname, 'Achternaam kan niet leeg zijn');
+                countError++;
+            }  else {
+                setSuccessFor(lastname);
+            }
+            if (emailValue === '') {
+                setErrorFor(email, 'Email kan niet leeg zijn');
+                countError++;
+            }  else {
                 setSuccessFor(email);
+            }
+            if (!isEmail(emailValue)) {
+                setErrorFor(email, 'Ongeldige email adres');
+                countError++;
+            } else {
+                setSuccessFor(email);
+            }
+
+            if (!EmailCheck()){
+                setErrorFor(email, 'Deze email is al geregistreerd, probeer in te loggen');
+                countError++;
+            }  else {
+                setSuccessFor(email);
+            }
+            if (passwordValue === '') {
+                setErrorFor(password, 'Wachtwoord kan niet leeg zijn');
+                countError++;
+            }  else {
                 setSuccessFor(password);
-                setSuccessFor(password2);
+            }
+            if (password2Value === '') {
+                setErrorFor(password2, 'Dit veld mag niet leeg zijn');
+                countError++;
+            }  else {
+                if (passwordValue !== password2Value) {
+                    setErrorFor(password2, 'Wachtwoorden komen niet overeen');
+                    countError++;
+                }  else {
+                    setSuccessFor(password2);
+                }
+            }
+
+            console.log("Count: " + countError);
+            if (countError === 0) {
                 SignUp();
             }
+
         }
 
         function setErrorFor(input, message) {
