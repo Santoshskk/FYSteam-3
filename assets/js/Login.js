3d18a969ac26908e58541e7aead2ef3914a6fb8c
othaimIbo <iboothaim@gmail.com>
2022-12-16 11:28:57 +0100

    Brought changes to the login & registration files

--- assets/js/Login.js
+++ assets/js/Login.js
@@ -3,77 +3,70 @@
  */
 
 
-document.addEventListener('DOMContentLoaded', start)
+window.onload = () => {
 
-function start() {
-    document.getElementById('login').addEventListener('click', getInfo);
-}
+    const form = document.getElementById('form');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
 
-let userEmail
-let userPassword
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
 
-function getInfo() {
-    userEmail = document.getElementById('user-username').value
-    userPassword = document.getElementById('user-password').value
-    formValidation(userEmail, userPassword);
-}
+        checkInputs(email, password);
 
-function getDatabse() {
+        function SignIn() {
+            FYSCloud.API.queryDatabase("SELECT * FROM user WHERE email = (?)", [email]
+            ).then(data => {
+                let psw = data[0].password;
+                let adm = data[0].isAdmin.data[0];
+                if (psw === password) {
+                    // start sesion - set session for user id & admin id.
+                    FYSCloud.Session.set("userID", data[0].userID);
+                    FYSCloud.Session.set("isAdmin", data[0].isAdmin.data[0]);
+                    if (adm === 1) {
+                        window.location.assign('admin/AdminDashboard.html')
+                    } else {
+                        window.location.assign('ProfilePage.html')
+                    }
+                } else {
+                    setError();
+                    e.preventDefault();
+                }
+            }).catch(err => {
+                setError();
+                console.log(err);
+            });
 
-
-    //Login function
-    FYSCloud.API.queryDatabase("SELECT * FROM user WHERE email = (?)", [userEmail]
-    ).then(data => {
-        let psw = data[0].password;
-        let adm = data[0].isAdmin.data[0];
-        if (psw === password) {
-            // start sesion - set session for user id & admin id.
-            FYSCloud.Session.set("userID", data[0].userID);
-            FYSCloud.Session.set("isAdmin", data[0].isAdmin.data[0]);
-            if (adm === 1) {
-                window.location.assign('admin/AdminDashboard.html')
-            } else {
-                window.location.assign('ProfilePage.html')
+            function setError(){
+                let message = "Controleer of je het juiste e-mailadres en wachtwoord gebruikt hebt en probeer het nog eens."
+                const small = document.querySelector('#psw-error');
+                small.parentElement.className = 'form-control error';
+                small.innerText = message;
             }
-        } else {
-            setError();
-            evt.preventDefault();
         }
-    }).catch(err => {
-        setError();
-        console.log(err);
+
+        function checkInputs(emailVal,passwordVal) {
+            let countError = 0;
+
+            if (emailVal === '') {
+                setEmpty(emailVal);
+                countError++;
+            }
+            if (passwordVal === '') {
+                setEmpty(passwordVal);
+                countError++;
+            }
+            if (countError === 0) {
+                SignIn();
+            }
+
+            function setEmpty(input){
+                let message = "Dit veld mag niet leeg zijn"
+                const formControl = input.parentElement;
+                const small = formControl.querySelector('small');
+                formControl.className = 'form-control error';
+                small.innerText = message;
+            }
+        }
     })
-}
-function formValidation(emailVal,passwordVal){
-
-    let countError = 0;
-
-    if (emailVal === '') {
-        setEmpty(userEmail);
-        countError++;
-    }
-    if (passwordVal === '') {
-        setEmpty(userPassword);
-        countError++;
-    }
-    if (countError === 0) {
-        getDatabse()
-    }
-
-    function setEmpty(input){
-        let message = "Dit veld mag niet leeg zijn"
-        const formControl = input.parentElement;
-        const small = formControl.querySelector('small');
-        formControl.className = 'form-control error';
-        small.innerText = message;
-    }
-
-    function setError(){
-        let message = "Controleer of je het juiste e-mailadres en wachtwoord gebruikt hebt en probeer het nog eens."
-        const small = document.querySelector('#psw-error');
-        small.parentElement.className = 'form-control error';
-        small.innerText = message;
-    }
-
-
 }
