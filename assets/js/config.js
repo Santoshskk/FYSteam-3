f276ca31516c01a1b05c9e5f85dcba39f1ede235
Tygo <tygo.geervliet@hva.nl>
2022-12-23 16:48:19 +0100

    ALLES WERKT 3. NIET MEER AANKOMEN

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,5 +5,5 @@ FYSCloud.API.configure({
     url: "https://api.fys.cloud",
     apiKey: "fys_is102_3.CCQFcAnNW1sV6uYi",
     database: "fys_is102_3_live",
-    environment: "mockup"
+    environment: "live"
 });
