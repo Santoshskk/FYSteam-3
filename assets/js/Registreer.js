0eb19396fba50762ae45033a5759ded1f2730e1b
Tygo <tygo.geervliet@hva.nl>
2022-12-23 15:29:16 +0100

    ALLES WERKT. NIET MEER AANKOMEN

--- assets/js/Registreer.js
+++ assets/js/Registreer.js
@@ -16,40 +16,40 @@ document.addEventListener('DOMContentLoaded', function (e) {
     form.addEventListener('submit', e => {
         e.preventDefault();
 
-        checkInputs(); // form validation
+
 
         function SignUp() {
-             FYSCloud.API.queryDatabase(
-                 "INSERT INTO user (isAdmin, firstName, lastName, email, password) VALUES (?, ?, ?, ?, ?);",
-                 [0, firstname.value, lastname.value, email.value, password.value]
-             ).then(() => {
-                 FYSCloud.API.queryDatabase(
-                     "SELECT userID FROM user WHERE email = (?)", [email.value]
-                 ).then(data => {
-                     console.log(data);
-                     let userID = data[0].userID;
-                     FYSCloud.API.queryDatabase(
-                         "INSERT INTO userinfo (userID, nationality, gender, age, discription) VALUES (?, ?, ?, ?, ?);",
-                         [userID, null, null, null, null]
-                     ).then(() => {
-                         console.log(data);
-                         FYSCloud.API.queryDatabase(
-                             "INSERT INTO tripinfo (userID, location, startDate, endDate) VALUES (?, ?, ?, ?);",
-                             [userID, null, null, null]
-                         ).then(() => {
-                             // window.location.assign('index.html');
-                         }).catch(err => {
-                             console.log(err);
-                         })
-                     }).catch(err => {
-                         console.log(err);
-                     })
-                 }).catch(err => {
-                     console.log(err);
-                 })
-             }).catch(err => {
-                 console.log(err);
-             })
+            FYSCloud.API.queryDatabase(
+                "INSERT INTO user (isAdmin, firstName, lastName, email, password, profileImage) VALUES (?, ?, ?, ?, ?,?);",
+                [0, firstname.value, lastname.value, email.value, password.value, "https://www.showflipper.com/blog/images/default.jpg"]
+            ).then(() => {
+                FYSCloud.API.queryDatabase(
+                    "SELECT userID FROM user WHERE email = (?)", [email.value]
+                ).then(data => {
+                    console.log(data);
+                    let userID = data[0].userID;
+                    FYSCloud.API.queryDatabase(
+                        "INSERT INTO userinfo (userID, nationality, gender, age, discription) VALUES (?, ?, ?, ?, ?);",
+                        [userID, null, null, null, null]
+                    ).then(() => {
+                        console.log(data);
+                        FYSCloud.API.queryDatabase(
+                            "INSERT INTO tripinfo (userID, location, startDate, endDate) VALUES (?, ?, ?, ?);",
+                            [userID, null, null, null]
+                        ).then(() => {
+                            window.location.assign('index.html');
+                        }).catch(err => {
+                            console.log(err);
+                        })
+                    }).catch(err => {
+                        console.log(err);
+                    })
+                }).catch(err => {
+                    console.log(err);
+                })
+            }).catch(err => {
+                console.log(err);
+            })
         }
 
         function checkInputs() {
@@ -57,8 +57,6 @@ document.addEventListener('DOMContentLoaded', function (e) {
             const firstnameValue = firstname.value.trim();
             const lastnameValue = lastname.value.trim();
             const emailValue = email.value.trim();
-            const passwordValue = password.value.trim();
-            const password2Value = password2.value.trim();
 
             let countError = 0;
 
@@ -71,58 +69,67 @@ document.addEventListener('DOMContentLoaded', function (e) {
             if (lastnameValue === '') {
                 setErrorFor(lastname, 'Dit veld mag niet leeg zijn');
                 countError++;
-            }  else {
+            } else {
                 setSuccessFor(lastname);
             }
 
             if (emailValue === '') {
                 setErrorFor(email, 'Dit veld mag niet leeg zijn');
                 countError++;
-            }  else if(!isEmail(emailValue)){
-                setErrorFor(email,'Geen geldige Email.');
+            } else if (!isEmail(emailValue)) {
+                setErrorFor(email, 'Geen geldige Email.');
                 countError++;
-            } else{
-                FYSCloud.API.queryDatabase(
+            } else {
+               return FYSCloud.API.queryDatabase(
                     "SELECT userID FROM user WHERE email = (?)", [email.value],
-                ).then(data => {
-                    console.log(data.length)
-                    if(data.length>0){
-                        setErrorFor(email,'Deze email is geregistreerd, probeer login');
-                        countError++;
-                    }else {
-                        setSuccessFor(email);
-                    }
-                })
+                );
             }
+//
+        }
+
+        const result = checkInputs();
+        result.then(function (data) {
+            countError = 0;
+            const passwordValue = password.value.trim();
+            const password2Value = password2.value.trim();
+
+
+            console.log(data.length)
+            if (data.length > 0) {
+                countError++;
+                console.log("SJOERD!")
+                setErrorFor(email, 'Deze email is geregistreerd, probeer login');
+                data.push(countError);
+            } else {
+                setSuccessFor(email);
+            }
+            //
 
             if (passwordValue === '') {
                 setErrorFor(password, 'Dit veld mag niet leeg zijn');
                 countError++;
-            }
-            else if(!passwordChecker(passwordValue)){
-                setPswError(password,'Wachtwoord moet bestaan uit minimaal 6 karakters,' +
+            } else if (!passwordChecker(passwordValue)) {
+                setPswError(password, 'Wachtwoord moet bestaan uit minimaal 6 karakters,' +
                     ' 1 hoofdletter, 1 kleine letter en 1 cijfer.')
                 countError++;
-            }else {
+            } else {
                 setSuccessFor(password);
             }
 
             if (password2Value === '') {
                 setErrorFor(password2, 'Dit veld mag niet leeg zijn');
                 countError++;
-            }  else if (passwordValue !== password2Value) {
-                     setErrorFor(password2, 'Wachtwoord komt niet overeen');
-                    countError++;
-                }  else {
-                    setSuccessFor(password2);
-                }
-
-            if (countError === 0) {
+            } else if (passwordValue !== password2Value) {
+                setErrorFor(password2, 'Wachtwoord komt niet overeen');
+                countError++;
+            } else {
+                setSuccessFor(password2);
+            }
+            if(data.length === 0 && countError === 0)   {
+                console.log("jajajaja")
                 SignUp();
             }
-
-            }
-
+        })
 
         function setErrorFor(input, message) {
             const formControl = input.parentElement;
@@ -137,21 +144,22 @@ document.addEventListener('DOMContentLoaded', function (e) {
             input.parentElement.className = 'form-control error';
             small.innerText = message;
         }
+
         function setSuccessFor(input) {
             const formControl = input.parentElement;
             formControl.className = 'form-control success';
         }
 
-          function EmailCheck(){
-               FYSCloud.API.queryDatabase(
-                  "SELECT userID FROM user WHERE email = (?)", [email.value],
-                  ).then(function (data){
-                  console.log(data)
-                  return data.length <= 0;
-              })
+        function EmailCheck() {
+            FYSCloud.API.queryDatabase(
+                "SELECT userID FROM user WHERE email = (?)", [email.value],
+            ).then(function (data) {
+                console.log(data)
+                return data.length <= 0;
+            })
 
 
-          }
+        }
 
 
         function isEmail(email) {
@@ -166,8 +174,9 @@ document.addEventListener('DOMContentLoaded', function (e) {
             }
 
         }
-        function passwordChecker(password){
-            let regexPasword= /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,255}$/
+
+        function passwordChecker(password) {
+            let regexPasword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,255}$/
             //Execution passwordcheck
             if (password.match(regexPasword)) {
                 console.log("valid");
